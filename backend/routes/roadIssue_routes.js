67ae90adabe4552e297fe4d5e762a3c102ce187b
drishtisubhedar <drishtiipad@gmail,com>
2024-02-24 03:57:19 +0530

    cloudinary added on roads

--- backend/routes/roadIssue_routes.js
+++ backend/routes/roadIssue_routes.js
@@ -2,33 +2,24 @@ const express = require("express")
 const roadIssueRouter = express.Router()
 const { roadIssueModel } = require("../models/roadIssueModel");
 const multer = require("multer");
+const uploadImage = require("../uploadImage");
 
-const storage = multer.diskStorage({
-    destination: function (req,file,cb){
-        return cb(null, "./roadIssueUploads")
-    },
-    filename: function (req,file,cb){
-        return cb(null, `${Date.now()}-${file.originalname}`)
-    }
-})
-
-const upload = multer({ storage })
-
-//for reading data
 roadIssueRouter.get("/roadissue",async(req, res) => {
     const data = await roadIssueModel.find({})
     res.json({success: true, data: data})
 });
 
-//create or post data
-roadIssueRouter.post("/roadissuecreate",upload.single("issueImage"),async(req,res)=>{
+roadIssueRouter.post("/roadissuecreate", async(req,res)=>{
+    uploadImage(req.body.image)
+        .then((url)=> res.send(url))
+        .catch((err)=> res.status(500).send(err))
     console.log(req.body)
-    console.log(req.file)
+    // console.log(req.file)
     const data = new roadIssueModel({
-        issueImage: {
-            data: req.file.filename,
-            contenType: "image"
-        },
+        // issueImage: {
+        //     data: req.file.filename,
+        //     contenType: "image"
+        // },
         issue: req.body.issue,
         issueDesc: req.body.issueDesc,
         issueLevel: req.body.issueLevel
@@ -36,8 +27,44 @@ roadIssueRouter.post("/roadissuecreate",upload.single("issueImage"),async(req,re
     await data.save()
     res.send({
         success: true, message: "data saved successfully"
-    })
+    })   
 })
+
+// const storage = multer.diskStorage({
+//     destination: function (req,file,cb){
+//         return cb(null, "./roadIssueUploads")
+//     },
+//     filename: function (req,file,cb){
+//         return cb(null, `${Date.now()}-${file.originalname}`)
+//     }
+// })
+
+// const upload = multer({ storage })
+
+// //for reading data
+// roadIssueRouter.get("/roadissue",async(req, res) => {
+//     const data = await roadIssueModel.find({})
+//     res.json({success: true, data: data})
+// });
+
+// //create or post data
+// roadIssueRouter.post("/roadissuecreate",upload.single("issueImage"),async(req,res)=>{
+    // console.log(req.body)
+    // console.log(req.file)
+    // const data = new roadIssueModel({
+    //     issueImage: {
+    //         data: req.file.filename,
+    //         contenType: "image"
+    //     },
+    //     issue: req.body.issue,
+    //     issueDesc: req.body.issueDesc,
+    //     issueLevel: req.body.issueLevel
+    // })
+    // await data.save()
+    // res.send({
+    //     success: true, message: "data saved successfully"
+    // })
+// })
 
 module.exports = {
     roadIssueRouter,
