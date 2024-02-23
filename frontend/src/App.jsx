089c50ac549130423ef95f3066dc79e8968fa686
Anusha Sanghavi <sanghavi29anusha@gmail.com>
2024-02-24 03:30:25 +0530

    sandlpage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,8 @@ import Road from "./pages/Road"
 import Clean from "./pages/Clean"
 import Card from "./components/Card"
 import AddData from "./components/AddData"
+import Login from "./pages/login"
+import SignUp from "./pages/Signup"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 
@@ -14,11 +16,13 @@ function App() {
     <>
       <BrowserRouter>
         <Routes>
-          <Route pcleanath='/' element={<Home />} />
+          <Route path='/' element={<Home />} />
           <Route path='/clean' element={<Clean/>} />
           <Route path='/food' element={<Food />} />
           <Route path='/road' element={<Road />} />
           <Route path="adddata" element={<AddData />} />
+          <Route path="login" element={<Login />} />
+          <Route path="signup" element={<SignUp />} />
         </Routes>
       </BrowserRouter>
     </>
---
+++ frontend/src/pages/Signup.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+export default function SignUp(){
+    return <div>
+        <section class="flex flex-col md:flex-row h-screen items-center">
+        <div class="bg-white w-full md:max-w-md lg:max-w-full md:mx-0 md:w-1/2 xl:w-1/3 h-screen px-6 lg:px-16 xl:px-12 flex items-center justify-center">
+        <div class="w-full h-100">
+                <h1 class="text-xl md:text-2xl font-bold leading-tight mt-12">Sign Up to your account</h1>
+            <form class="mt-6" action="#" method="POST">
+            <div>
+                <label class="block text-gray-700">Email Address</label>
+                    <input type="email" name="" id="" onChange={(e) => setEmail(e.target.value)} placeholder="Enter Email Address" class="w-full px-4 py-3 rounded-lg bg-gray-200 mt-2 border focus:border-blue-500 focus:bg-white focus:outline-none" autofocus autocomplete required></input>
+            </div>
+            <div class="mt-4">
+                <label class="block text-gray-700">Password</label>
+                    <input type="password" name="" id="" onChange={(e) => setPassword(e.target.value)} placeholder="Enter Password" minlength="6" class="w-full px-4 py-3 rounded-lg bg-gray-200 mt-2 border focus:border-blue-500 focus:bg-white focus:outline-none" required></input>
+            </div>
+            <div class="text-right mt-2">
+             </div>
+            <button type="submit" onClick={SignUp} class="w-full block bg-gray-800 hover:bg-grey-400 focus:bg-grey-400 text-white font-semibold rounded-lg px-4 py-3 mt-6">Sign Up</button>
+            </form>
+            <p class="mt-8">Already have an account? <a href="#" class="text-blue-500 hover:text-blue-700 font-semibold">Log In</a></p>
+            <br></br><p><a href="#" class="text-blue-500 hover:text-blue-700 font-semibold">Admin</a></p>
+        </div>
+        </div>
+        </section>
+    </div>
+    }
